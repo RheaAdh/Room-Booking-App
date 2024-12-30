eb033bfe0eb94799745a420549afd776eff3b126
Rhea Adhikari <rheadhikari@gmail.com>
2024-12-30 18:26:23 +0530

    Initial commit

---
+++ server/src/test/java/com/example/profpride/ProfprideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profpride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfprideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
