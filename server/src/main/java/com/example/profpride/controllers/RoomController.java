def4ef1257515517d4e5367c0d49ac2b50418905
Rhea Adhikari <51349679+RheaAdh@users.noreply.github.com>
2025-10-17 22:08:01 +0530

    Update RoomController.java

--- server/src/main/java/com/example/profpride/controllers/RoomController.java
+++ server/src/main/java/com/example/profpride/controllers/RoomController.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8081", "exp://192.168.1.12:8081"})
 public class RoomController {
 
     @Autowired
